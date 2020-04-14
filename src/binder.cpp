661141714b37e0673d0d1aa9087b32eb2233fb25
Antonio Mallia <me@antoniomallia.it>
2020-04-14 19:01:54 +0200

    Initial implementation

---
+++ src/binder.cpp
@@ -0,0 +1,33 @@
+#include <boost/algorithm/string.hpp>
+#include <forward_index_builder.hpp>
+#include <pybind11/pybind11.h>
+
+namespace py = pybind11;
+
+using pisa::Document_Record;
+using pisa::Forward_Index_Builder;
+
+static std::size_t const THREADS = 2;
+
+void parse(std::string const &fwd, std::string const &format,
+           std::size_t batch_size = 10'000) {
+  pisa::Forward_Index_Builder fwd_builder;
+  fwd_builder.build(
+      std::cin, fwd,
+      [](std::istream &in) -> std::optional<Document_Record> {
+        std::string line;
+        if (std::getline(in, line) && not line.empty()) {
+          return std::make_optional<Document_Record>("id", "text", "");
+        }
+        return std::nullopt;
+      },
+      [](std::string &&term) -> std::string {
+        boost::algorithm::to_lower(term);
+        return std::move(term);
+      },
+      pisa::parse_plaintext_content, batch_size, THREADS);
+}
+
+PYBIND11_MODULE(pysa, m) {
+  m.def("parse", &parse);
+}
---
+++ test/test.py
@@ -0,0 +1,3 @@
+import pysa
+
+assert pysa.__version__ == '0.1.0'
